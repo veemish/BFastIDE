be7464ce59dd67ae9ca676d4e72e2ba5eb79bd43
Joshua <mama27j@gmail.com>
2019-07-27 00:01:05 +0300

    add working directory

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/version', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({version:'1.0.0', name:'ethan'});
+});
+
+router.get('/help', function (request, response) {
+  response.json({
+    version:{
+      method: 'GET',
+      endpoint: '/version',
+      description: 'Check version of the current tool'
+    },
+    create: {
+      method: 'POST',
+      endpoint: '/domain',
+      description: 'Creat a domain from a JSON file'
+    }
+  });
 });
 
 module.exports = router;
---
+++ spring/README.md
@@ -0,0 +1,8 @@
+# Spring Boot
+This folder contain the spring bootstrapping application for DaaS. If the 
+application is not available in a working directory it will be fetched from 
+github.
+
+## Warning
+Do not try to edit the script called `init` found in this folder its used to 
+pull project from github
